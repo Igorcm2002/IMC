ea25083ad1759648bc6b25e45eac25404e1097b3
Icmartins <icm260102@gmail.com>
2019-08-27 22:16:55 -0300

    IMC com imagem

--- app/src/main/java/com/example/imc/MainActivity.java
+++ app/src/main/java/com/example/imc/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         btCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ImageView img = findViewById(R.id.imageView);
+                img.setImageResource(R.drawable.perfil);
                 TextView editPeso = (TextView) findViewById(R.id.editPeso);
                 TextView editAltura = (TextView) findViewById(R.id.editAltura);
                 TextView tvResultado = (TextView) findViewById(R.id.tvResultado);
@@ -28,23 +31,41 @@ public class MainActivity extends AppCompatActivity {
                 if(imc[0]<18.5) {
                     tvResultado.setText(imc[0] + "");
                     tvDescricao.setText("Está Abaixo do Peso");
+                    img.setImageResource(R.drawable.abaixopeso);
                 }else{
                     if(imc[0]<25) {
                         tvResultado.setText(imc[0] + "");
                         tvDescricao.setText("Está Com o Peso Ideal ");
+                        img.setImageResource(R.drawable.normal);
                     }else{
                         if(imc[0]<30) {
                             tvResultado.setText(imc[0] + "");
                             tvDescricao.setText("Está Acima do peso");
+                            img.setImageResource(R.drawable.sobrepeso);
                     }else{
-                            if(imc[0]>=30) {
+                            if(imc[0]<30) {
                                 tvResultado.setText(imc[0] + "");
-                                tvDescricao.setText("Está Com Obesidade");
+                                tvDescricao.setText("Está Com Obesidade 1");
+                                img.setImageResource(R.drawable.obesidade1);
+
+                            }else{
+                                if(imc[0]<39) {
+                                    tvResultado.setText(imc[0] + "");
+                                    tvDescricao.setText("Está Com Obesidade 2");
+                                    img.setImageResource(R.drawable.obesidade2);
+
+                                }else{
+                                    if(imc[0]>=40) {
+                                        tvResultado.setText(imc[0] + "");
+                                        tvDescricao.setText("Está Com Obesidade 3");
+                                        img.setImageResource(R.drawable.obesidade3);
                             }
                         }
                     }
                 }
             };
-            });
+            };
+        };
+    } );
     }
 }
